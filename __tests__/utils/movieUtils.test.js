651bb24b5251fdf2224427620eeb135dfa1bd8e5
sara woodford <sarawoodford6@gmail.com>
2024-10-31 00:37:19 -0230

    test: MovieUtils.test
    - installed jest
    - Added test to ensure getMoviesByGenre returns
    specified x amount of movies, all with the same
    specified genre.
    - Added test to ensure that appropriate response
    is returned when there are no movies in specified
    genre

--- __tests__/utils/movieUtils.test.js
+++ __tests__/utils/movieUtils.test.js
@@ -5,9 +5,27 @@ const {
      selectRandomMovieId
 } = require("../../utils/movieUtils");
 
+const { Genres, Movies } = require('../../utils/data');
+
 describe('Movie Utility Functions', () => {
     describe('getMoviesByGenre', () => {
+        test('should return (x) amount of movies from given genre', () => {
+            // specify function parameters 
+            const genre = Genres.CRIME;
+            const x = 3;
+            const result = getMoviesByGenre(genre, x);
 
+            // expect 4 (x) movies to be returned and all to be crime (genre)
+            expect(result.length).toBe(x);
+            expect(result.every(movie => movie.genre === genre)).toBe(true);
+        });
+
+        test('should return an appropriate response (empty array) if genre has no movies', () => {
+            // include a genre that doesn't exist as the genre parameter
+            const result = getMoviesByGenre('DOES-NOT-EXIST', 5);
+            // expect result to be an empty array
+            expect(result).toEqual([]);
+        })
     });
 
     describe('getTopRatedMovies', () => {
@@ -24,4 +42,3 @@ describe('Movie Utility Functions', () => {
 });
 
 
-getRandomMoviesByGenre
